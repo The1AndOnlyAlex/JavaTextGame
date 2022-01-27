dcfef3dda5c4e3a7b42018b1be1e5118452740a4
Alex Duan <40921264+The1AndOnlyAlex@users.noreply.github.com>
2022-01-26 23:07:45 -0800

    Updated tutorial.java
    1/26/2022

--- tutorial.java
+++ tutorial.java
@@ -1,61 +1,77 @@
 //import java.util.Scanner;
 import java.util.*;
-import java.io.*;
 
 public class tutorial {
 
-    static String dialogue = "empty";
-    static String name = "Tu";
+    static String input = "empty";
+
+    static Scanner player = new Scanner(System.in);
+
+    public static void CloseScanner()
+    {
+        player.close();
+    }
 
     public static String Intro() {
 
-        Scanner player = new Scanner(System.in);
+       
         System.out.println("Hello there aspiring rapper! (Enter to continue)");
-        dialogue = player.nextLine();
+        input = player.nextLine();
+
         System.out.println("I see you're trying to make a name for yourself.");
-        dialogue = player.nextLine();
+        input = player.nextLine();
         System.out.println("Well first off, why don't you tell me your rapper name? ");
         System.out.println("Enter name: ");
-        name = player.nextLine();
-        name = name.replaceAll("\\s", "");
-        while(name.isEmpty()) {
+        input = player.nextLine();
+        input = input.replaceAll("\\s", "");
+        while(input.isEmpty()) {
             System.out.println("Seriously, what's your name?");
             System.out.println("Enter name: ");
-            name = player.nextLine();
-            name = name.replaceAll("\\s", "");
+            input = player.nextLine();
+            input = input.replaceAll("\\s", "");
         }
-        return name;
-        
+        //player.close();
+        return input;
+
     }
 
-    public static void I() {
+    public static String I() {
 
-        Scanner player = new Scanner(System.in);
+        //Scanner player = new Scanner(System.in);
         System.out.println(
                     "Let me give you the basic rundown on how you can get started on rapping right away.");
-        dialogue = player.nextLine();
+        input = player.nextLine();
         System.out.println(
                     "The first thing you need to know is how to check your stats. Your stats include your ID, Fame, Cash, Skill, and more.");
-        dialogue = player.nextLine();
-        System.out.println("Stats: Enter \"1\" Practice: Enter \"2\" \n Move: Enter \"3\" \n Purchase: Enter \"4\" \n Record Label: Enter \"5\"");
+        input = player.nextLine();
+        System.out.println("Stats: Enter \"1\" \n Practice: Enter \"2\" \n Move: Enter \"3\" \n Purchase: Enter \"4\" \n Record Label: Enter \"5\"");
         System.out.println("Check your current stats right now by typing in \"1\". \n");
+
+        while(input != "1") {
+            System.out.println("Please type in 1 for now.");
+            input = player.nextLine();
+        }
+        //player.close();
+        return input;
 
     }
 
     public static void LastDialogue() {
 
-        Scanner player = new Scanner(System.in);
+        //Scanner player = new Scanner(System.in);
         System.out.println("\n Great!");
-        dialogue = player.nextLine();
+        input = player.nextLine();
         System.out.println("\n Well it looks like you need a bit more practice with your rapping skills.");
-        dialogue = player.nextLine();
+        input = player.nextLine();
         System.out.println("You can practice whenever you want.");
-        dialogue = player.nextLine();
+        input = player.nextLine();
         System.out.println("Just head home by typing 'p.move(home)'.");
-        dialogue = player.nextLine();
+        input = player.nextLine();
         System.out.println("Then you can practice your rapping by then typing 'p.practice(rap)'.");
-        dialogue = player.nextLine();
+        input = player.nextLine();
         System.out.println("Well, looks like you're good to go! Make sure to keep an eye on time and come meet me at the club at 21 (Military time) by typing in 'p.go(club)'.");
+        //player.close();
+
     }
     
 }

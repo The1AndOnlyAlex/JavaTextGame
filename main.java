b61d66aa67f78f9dacff6cd42062859983fa3865
Alex Duan <40921264+The1AndOnlyAlex@users.noreply.github.com>
2022-02-02 23:10:51 -0800

    Updated main.java
    2/2/2022

--- main.java
+++ main.java
@@ -3,7 +3,7 @@
 public class main {
 
     static String input = "empty";
-    static boolean gameEnd = false;
+    static boolean gameGoing = true;
 
     public static void main(String[] args) {
 
@@ -16,7 +16,7 @@ public class main {
         player.stats();
         tutorial.Intro3();
         
-        while(gameEnd != false) {
+        while(gameGoing = true) {
             player.action();
         }
 
@@ -29,15 +29,18 @@ public class main {
 
 
         /*
-        1/25/2022: Get rid of the majority of the static keywords in tutorial and player
-
-
-        Make sure to put this entire tutorial into a seperate class/function and just run it here in main. In addition, make sure to actually TAKE IN the user input so that they can type in what action they want to carry out.
         To do list:
+
+        1/25/2022: Get rid of the majority of the static keywords in tutorial and player
         -Add weekly competition on Friday nights
-        -Make a way to be signed into a record label through fame and money
         -Random encounters with street thugs who can steal your money.
         -Win this game demo by reaching a certain fame level and then challenging and beating the champion rapper.
+
+
+        2/2/2022:
+        -Add SIXTH option to check the time.
+        -Tell user the current time if the place they want to go is closed
+        -Make an easier way to make money (DONATIONS + WORK)
         */
 
 
